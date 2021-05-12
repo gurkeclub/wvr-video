959c043fe973593b8cba3a62bbf4be60fbff9ab0
champyrti <champi@gurke.club>
2021-05-12 19:19:02 +0200

    Switch from raw encoding to lossless x264 encoding

--- src/encoder.rs
+++ src/encoder.rs
@@ -2,11 +2,11 @@ use std::io::Write;
 
 use anyhow::{Context, Result};
 
-use gst;
-use gst::prelude::*;
-use gst::State;
-use gst_app;
-use gst_video;
+use gst::{self, Format, Fraction};
+use gst::{prelude::*, Buffer};
+use gst::{Element, ElementFactory, Pipeline, State};
+use gst_app::{self, AppSrc};
+use gst_video::{self, VideoFormat, VideoInfo};
 
 pub enum TextureFormat {
     RGBU8,
@@ -16,8 +16,8 @@ pub enum TextureFormat {
 }
 
 pub struct VideoEncoder {
-    pipeline: gst::Pipeline,
-    app_src: gst_app::AppSrc,
+    pipeline: Pipeline,
+    app_src: AppSrc,
 }
 
 impl VideoEncoder {
@@ -29,28 +29,36 @@ impl VideoEncoder {
             path.to_owned()
         };
 
-        let pipeline = gst::Pipeline::new(None);
-        let appsrc = gst::ElementFactory::make("appsrc", None).unwrap();
-        let videoconvert = gst::ElementFactory::make("videoconvert", None).unwrap();
-        let queue = gst::ElementFactory::make("queue", None).unwrap();
+        let pipeline = Pipeline::new(None);
 
-        let avimux = gst::ElementFactory::make("avimux", None).unwrap();
-        let sink = gst::ElementFactory::make("filesink", None).unwrap();
+        let appsrc = ElementFactory::make("appsrc", None).unwrap();
+
+        let videoconvert = ElementFactory::make("videoconvert", None).unwrap();
+
+        let videoflip = ElementFactory::make("videoflip", None).unwrap();
+        videoflip.set_property("method", &"vertical-flip").unwrap();
+
+        let queue = ElementFactory::make("queue", None).unwrap();
+
+        let x264enc = ElementFactory::make("x264enc", None).unwrap();
+        x264enc.set_property("qp-max", &0u32).unwrap();
+
+        let avimux = ElementFactory::make("avimux", None).unwrap();
+        let sink = ElementFactory::make("filesink", None).unwrap();
         sink.set_property("location", &path).unwrap();
 
         pipeline
-            .add_many(&[&appsrc, &videoconvert, &queue, &avimux, &sink])
+            .add_many(&[&appsrc, &videoconvert, &queue, &x264enc, &avimux, &sink])
             .unwrap();
-        gst::Element::link_many(&[&appsrc, &videoconvert, &queue, &avimux, &sink]).unwrap();
+        Element::link_many(&[&appsrc, &videoconvert, &queue, &x264enc, &avimux, &sink]).unwrap();
 
-        let appsrc = appsrc.dynamic_cast::<gst_app::AppSrc>().unwrap();
-        let info =
-            gst_video::VideoInfo::builder(gst_video::VideoFormat::Rgb, width as u32, height as u32)
-                .fps(gst::Fraction::new((framerate * 1000.0) as i32, 1000))
-                .build()
-                .unwrap();
+        let appsrc = appsrc.dynamic_cast::<AppSrc>().unwrap();
+        let info = VideoInfo::builder(VideoFormat::Rgb, width as u32, height as u32)
+            .fps(Fraction::new((framerate * 1000.0) as i32, 1000))
+            .build()
+            .unwrap();
         appsrc.set_caps(Some(&info.to_caps().unwrap()));
-        appsrc.set_property_format(gst::Format::Time);
+        appsrc.set_property_format(Format::Time);
 
         pipeline.set_state(State::Playing).context(format!(
             "Failed to start gstreamer encoder for output {:?}",
@@ -64,26 +72,27 @@ impl VideoEncoder {
     }
 
     pub fn stop(&mut self) {
-        if let Err(e) = self.pipeline.set_state(State::Paused) {
+        if let Err(e) = self.pipeline.set_state(State::Null) {
             eprintln!("Failed to stop video encoding: {:?}", e);
         }
         if let Err(e) = self.app_src.end_of_stream() {
             eprintln!("Failed to end stream: {:?}", e);
         }
-        println!("Pipeline stopped");
     }
 
     pub fn encode_frame(&mut self, time: f64, frame: &[u8]) {
-        let mut buffer = gst::Buffer::with_size(frame.len()).unwrap();
+        let pts = (time * 1_000_000.0) as u64 * gst::NSECOND;
+        let mut buffer = Buffer::with_size(frame.len()).unwrap();
         {
             let buffer = buffer.get_mut().unwrap();
-            buffer.set_pts((time * 1000.0) as u64 * gst::MSECOND);
+            buffer.set_pts(pts);
 
             let mut data = buffer.map_writable().unwrap();
             let mut data = data.as_mut_slice();
 
             data.write_all(frame).unwrap();
         }
+
         if let Err(error) = self.app_src.push_buffer(buffer) {
             panic!("{:?}", error);
         }
